b14f78a703191f9461d5bbdb139ba28c72b892ba
Nik Charlebois <nik_charlebois@hotmail.com>
2020-06-10 12:14:10 -0400

    Initial

--- Modules/MSCloudLoginAssistant/MSCloudLoginAssistant.psd1
+++ Modules/MSCloudLoginAssistant/MSCloudLoginAssistant.psd1
@@ -85,7 +85,8 @@
     #FunctionsToExport = ''
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-    CmdletsToExport        = @('Test-MSCloudLogin')
+    CmdletsToExport        = @('Test-MSCloudLogin', `
+                               'Invoke-MSCloudLoginMicrosoftGraphAPI')
 
     # Variables to export from this module
     VariablesToExport      = '*'
--- Modules/MSCloudLoginAssistant/Workloads/MicrosoftGraph.psm1
+++ Modules/MSCloudLoginAssistant/Workloads/MicrosoftGraph.psm1
@@ -2,18 +2,27 @@ function Connect-MSCloudLoginMicrosoftGraph
 {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true)]
+        [Parameter()]
+        [System.Management.Automation.PsCredential]
+        $CloudCredential,
+
+        [Parameter()]
         [System.String]
         $ApplicationId,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter()]
         [System.String]
         $TenantId,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter()]
         [System.String]
         $CertificateThumbprint
     )
+    if ($null -ne $CloudCredential)
+    {
+        Connect-MSCloudLoginGraphWithCredentials -CloudCredential $CloudCredential `
+            -ApplicationId $ApplicationId
+    }
     try
     {
         Write-Verbose "ICI"
@@ -31,4 +40,79 @@ function Connect-MSCloudLoginMicrosoftGraph
         Write-Verbose -Message $_
         throw $_
     }
+}
+
+function Connect-MSCloudLoginMicrosoftGraphWithCredential
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        $CloudCredential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId = "14d82eec-204b-4c2f-b7e8-296a70dab67e" #PoSh Graph SDK
+    )
+    if ($null -eq $Global:MSCloudLoginGraphAccessToken)
+    {
+        $azuretenantADName = $CloudCredential.UserName.Split('@')[1]
+
+        #Authority to Azure AD Tenant
+        $AzureADAuthority = "https://login.microsoftonline.com/$azuretenantADName/oauth2/v2.0/authorize"
+
+        #Resource URI to the Microsoft Graph
+        $resourceURL = "https://graph.microsoft.com/"
+
+        # Create UserCredential object
+        $accessToken = Get-AccessToken -TargetUri $resourceUrl `
+            -AuthUri $AzureADAuthority `
+            -ClientId $ApplicationId `
+            -Credentials $CloudCredential
+        $Global:MSCloudLoginGraphAccessToken = $accessToken
+    }
+}
+
+function Invoke-MSCloudLoginMicrosoftGraphAPI
+{
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        $CloudCredential,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $Uri,
+
+        [Parameter()]
+        [System.String]
+        $Body,
+
+        [Parameter()]
+        [System.Collections.Hashtable]
+        $Headers,
+
+        [Parameter()]
+        [System.String]
+        $Method,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId
+    )
+    $VerbosePreference = 'Continue'
+    Connect-MSCloudLoginMicrosoftGraphWithCredential -CloudCredential $CloudCredential
+    $requestHeaders = @{
+        "Authorization" = "Bearer " + $Global:MSCloudLoginGraphAccessToken
+        "Content-Type" = "application/json"
+    }
+    foreach ($key in $Headers.Keys)
+    {
+        Write-Verbose -Message "Adding Header {$key}"
+        $requestHeaders.Add($key, $Headers.$key)
+    }
+    $Result = Invoke-RestMethod -Method $Method -Headers $requestheaders -Uri $Uri -Body $Body
+    return $result
 }
