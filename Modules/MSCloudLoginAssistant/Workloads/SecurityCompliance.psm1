e16138c19272cea17f7d0fe1b0fe15a0604e0eff
Ole-Martin Heggen <ole-ma@outlook.com>
2021-03-01 13:41:38 +0100

    Fixed check for active sessions

--- Modules/MSCloudLoginAssistant/Workloads/SecurityCompliance.psm1
+++ Modules/MSCloudLoginAssistant/Workloads/SecurityCompliance.psm1
@@ -29,7 +29,6 @@ function Connect-MSCloudLoginSecurityCompliance
     $VerbosePreference = "Continue"
     $WarningPreference = 'SilentlyContinue'
     $ProgressPreference = 'SilentlyContinue'
-    [array]$activeSessions = Get-PSSession | Where-Object -FilterScript {$_.ComputerName -like '*.ps.compliance.protection*' -and $_.State -eq 'Opened'}
     Write-Verbose "$(Get-Runspace | Out-String)"
     [array] $opened = Get-Runspace | Where-Object -FilterScript {$_.RunspaceAvailability -eq 'Available'}
     for ($i = 1; $i -lt $opened.Length; $i++)
@@ -38,6 +37,7 @@ function Connect-MSCloudLoginSecurityCompliance
         $opened[$i].Close()
         $opened[$i].Dispose()
     }
+    [array]$activeSessions = Get-PSSession | Where-Object -FilterScript {$_.ComputerName -like '*.ps.compliance.protection*' -and $_.State -eq 'Opened'}
     if ($activeSessions.Length -ge 1 -and $SkipModuleReload -eq $true)
     {
         Write-Verbose -Message "Found {$($activeSessions.Length)} existing Security and Compliance Session"
