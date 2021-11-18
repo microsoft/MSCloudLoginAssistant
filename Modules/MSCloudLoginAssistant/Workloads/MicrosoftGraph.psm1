8d1ee8ffc050d3346baba79c8bf563a611afb856
Nik Charlebois <nik_charlebois@hotmail.com>
2021-11-17 19:38:12 -0500

    Update MicrosoftGraph.psm1

--- Modules/MSCloudLoginAssistant/Workloads/MicrosoftGraph.psm1
+++ Modules/MSCloudLoginAssistant/Workloads/MicrosoftGraph.psm1
@@ -10,9 +10,17 @@ function Connect-MSCloudLoginMicrosoftGraph
     # If the current profile is not the same we expect, make the switch.
     $currentProfile = (Get-MgProfile).Name
     Write-Verbose -Message "Current Profile: $currentProfile"
+    Write-Verbose -Message "Requested Profile: $($Global:MSCloudLoginConnectionProfile.MicrosoftGraph.ProfileName)"
     if ($Global:MSCloudLoginConnectionProfile.MicrosoftGraph.ProfileName -ne $currentProfile)
     {
+        Write-Verbose -Message "There are currently {$((dir function: | measure).Count) functions}"
+        Write-Verbose -Message "Removing Graph Modules from Runspace"
+        Remove-Module Microsoft.Graph.* -Force
+        Write-Verbose -Message "There are now {$((dir function: | measure).Count) functions}"
+
+        Write-Verbose -Message "Switching to Beta profile"
         Select-MgProfile $Global:MSCloudLoginConnectionProfile.MicrosoftGraph.ProfileName | Out-Null
+        Write-Verbose -Message "There are {$((dir function: | measure).Count) functions}"
         $Global:MSCloudLoginConnectionProfile.MicrosoftGraph.Connected = $false
     }
 
