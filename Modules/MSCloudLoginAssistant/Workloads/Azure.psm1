9da2f5f4947bee6b9d911f440fb0ed2c84a7d2b7
Nik Charlebois <nicharl@microsoft.com>
2024-09-10 13:57:51 -0400

    Release 1.1.22

--- Modules/MSCloudLoginAssistant/Workloads/Azure.psm1
+++ Modules/MSCloudLoginAssistant/Workloads/Azure.psm1
@@ -16,7 +16,6 @@ function Connect-MSCloudLoginAzure
             Write-Verbose -Message "Azure Connection Profile = $($Global:MSCloudLoginConnectionProfile.Azure | Out-String)"
             try
             {
-                Clear-AzContext | Out-Null
                 Connect-AzAccount -ApplicationId $Global:MSCloudLoginConnectionProfile.Azure.ApplicationId `
                                 -TenantId $Global:MSCloudLoginConnectionProfile.Azure.TenantId `
                                 -CertificateThumbprint $Global:MSCloudLoginConnectionProfile.Azure.CertificateThumbprint `
@@ -30,6 +29,77 @@ function Connect-MSCloudLoginAzure
             $Global:MSCloudLoginConnectionProfile.Azure.Connected = $true
             $Global:MSCloudLoginConnectionProfile.Azure.MultiFactorAuthentication = $false
             Write-Verbose -Message "Successfully connected to Azure using AAD App {$ApplicationID}"
+        }
+        catch
+        {
+            throw $_
+        }
+    }
+    elseif ($Global:MSCloudLoginConnectionProfile.Azure.AuthenticationType -eq 'CredentialsWithApplicationId' -or
+                $Global:MSCloudLoginConnectionProfile.Azure.AuthenticationType -eq 'Credentials' -or
+                $Global:MSCloudLoginConnectionProfile.Azure.AuthenticationType -eq 'CredentialsWithTenantId')
+    {
+        try
+        {
+            if ([System.String]::IsNullOrEmpty($Global:MSCloudLoginConnectionProfile.Azure.TenantId))
+            {
+                $Global:MSCloudLoginConnectionProfile.Azure.TenantId = $Global:MSCloudLoginConnectionProfile.Azure.Credentials.UserName.Split('@')[1]
+            }
+            Write-Verbose -Message "Attempting to connect to Azure using Credentials"
+            Connect-AzAccount -Credential $Global:MSCloudLoginConnectionProfile.Azure.Credentials `
+                              -Environment $Global:MSCloudLoginConnectionProfile.Azure.EnvironmentName `
+                              -ErrorAction Stop | Out-Null
+            $Global:MSCloudLoginConnectionProfile.Azure.ConnectedDateTime = [System.DateTime]::Now.ToString()
+            $Global:MSCloudLoginConnectionProfile.Azure.Connected = $true
+            $Global:MSCloudLoginConnectionProfile.Azure.MultiFactorAuthentication = $false
+            Write-Verbose -Message "Successfully connected to Azure using Credentials"
+        }
+        catch
+        {
+            try
+            {
+                Write-Verbose -Message "Attempting to connect to Azure using Credentials (MFA)"
+                Connect-AzAccount
+                $Global:MSCloudLoginConnectionProfile.Azure.ConnectedDateTime = [System.DateTime]::Now.ToString()
+                $Global:MSCloudLoginConnectionProfile.Azure.Connected = $true
+                $Global:MSCloudLoginConnectionProfile.Azure.MultiFactorAuthentication = $true
+                Write-Verbose -Message "Successfully connected to Azure using Credentials (MFA)"
+            }
+            catch
+            {
+                throw $_
+            }
+        }
+    }
+    elseif ($Global:MSCloudLoginConnectionProfile.Azure.AuthenticationType -eq 'AccessTokens')
+    {
+        Write-Verbose -Message "Attempting to connect to Azure using Access Token"
+        Connect-AzAccount -Tenant $Global:MSCloudLoginConnectionProfile.Azure.TenantId `
+                          -Environment $Global:MSCloudLoginConnectionProfile.Azure.EnvironmentName `
+                          -AccessToken $Global:MSCloudLoginConnectionProfile.Azure.AccessTokens | Out-Null
+        $Global:MSCloudLoginConnectionProfile.Azure.ConnectedDateTime = [System.DateTime]::Now.ToString()
+        $Global:MSCloudLoginConnectionProfile.Azure.Connected = $true
+        $Global:MSCloudLoginConnectionProfile.Azure.MultiFactorAuthentication = $false
+        Write-Verbose -Message "Successfully connected to Azure using Access Token"
+    }
+    elseif ($Global:MSCloudLoginConnectionProfile.Azure.AuthenticationType -eq 'Identity')
+    {
+        Write-Verbose -Message 'Attempting to connect to Azure using Managed Identity'
+        try
+        {
+            if ($NULL -eq $Global:MSCloudLoginConnectionProfile.OrganizationName)
+            {
+                $Global:MSCloudLoginConnectionProfile.OrganizationName = Get-MSCloudLoginOrganizationName -Identity
+            }
+
+            Connect-AzAccount-TenantId $Global:MSCloudLoginConnectionProfile.OrganizationName `
+                -Identity `
+                -EnvironmentName $Global:MSCloudLoginConnectionProfile.Azure.EnvironmentName | Out-Null
+
+            $Global:MSCloudLoginConnectionProfile.ExchangeOnline.ConnectedDateTime = [System.DateTime]::Now.ToString()
+            $Global:MSCloudLoginConnectionProfile.ExchangeOnline.Connected = $false
+            $Global:MSCloudLoginConnectionProfile.ExchangeOnline.MultiFactorAuthentication = $false
+            Write-Verbose -Message 'Successfully connected to Azure using Managed Identity'
         }
         catch
         {
